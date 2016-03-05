26edc08e6fad82e2206e25ec7e10539098bd3c8d
ruifranco <rui.franco@gmail.com>
2016-03-05 13:02:39 +0000

    [ADD] crm_lead_url
    Adds a url field to leads and opportunities

---
+++ crm_lead_url/__openerp__.py
@@ -0,0 +1,36 @@
+#! -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2016 Odooveloper (Rui Pedrosa Franco) All Rights Reserved
+#    http://pt.linkedin.com/in/ruipedrosafranco
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name'          : 'CRM Lead URL',
+    'version'       : '1.0',
+    'category'      : 'Customer Relationship Management',
+    'summary'       : 'Adds a URL field to leads and opportunities',
+    'description'   : 'Adds a URL field to leads and opportunities',
+    'author'        : 'Rui Pedrosa Franco',
+    'website'       : 'http://www.odooveloper.com',
+    'depends'       : ['crm'],
+    'data'          : ['views/crm_lead_view.xml'],
+    'installable'   : True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
