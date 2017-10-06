41a2abfe59ff651d67faef77a33dfe2fb1755785
ruifranco <rui.franco@gmail.com>
2017-10-06 15:40:52 +0100

    [ADD] l10n_pt_AO97_fix

---
+++ l10n_pt_AO97_fix/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'AO97 - correção de erros em pt_PT',
+    'version': '1.0',
+	'summary': 'Correção de erros na aplicação do AO97',
+	'description': """
+Correções à tradução pt_PT relativas a erros de aplicação do Acordo Ortográfico de 1997:
+
+'Contato' => 'Contacto'
+""",
+    'author': 'Odooveloper',
+    'website': 'http://www.odooveloper.com',
+    'depends': [],
+    'data': ['setup.xml'],
+	'installable': True,
+}
